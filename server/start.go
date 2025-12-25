41a7b146776f8d9334c6af9ec3f96d6ed3619a93
Nurpolat Yerkinbayev <nurbilim.kz@gmail.com>
2025-12-25 22:45:11 +0500

    Add external server package to be used by external projects. This is experimental commit to test module distribution

---
+++ server/start.go
@@ -0,0 +1,53 @@
+package server 
+
+import (
+	"fmt"
+
+	"github.com/nturbo1/reverse-proxy/internal/configs"
+	"github.com/nturbo1/reverse-proxy/internal/log"
+	"github.com/nturbo1/reverse-proxy/internal/server"
+)
+
+func Start() error {
+	appConfigs, env, err := setUpConfigsAndEnv()
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Initializing a logger...")
+	log.InitLogger(appConfigs.LogLevel)
+	log.Debug("Configurations: %s", appConfigs)
+	log.Debug("Environment: %s", env)
+	
+	server := server.NewServer(appConfigs, env)
+	log.Info("Listening on port %d...", appConfigs.Server.Port)
+	err = server.ListenAndServe()
+	log.Fatal("%s", err)
+	return err
+}
+
+func setUpConfigsAndEnv() (*configs.AppConfigs, *configs.Environment, error) {
+	appConfigs, err := configs.InitAppConfigs()
+	if err != nil {
+		fmt.Printf("Failed to initialize app configs due to: %s\n", err)
+		return nil, nil, err
+	}
+
+	env, err := configs.GetEnv(appConfigs)
+	if err != nil {
+		fmt.Printf("Failed to set up the Environment due to: %s\n", err)
+		return nil, nil, err
+	}
+	fmt.Printf("Environment: %v\n", env)
+
+	fmt.Printf("Raw app configs: %s\n", appConfigs)
+	fmt.Println("Replacing variable occurrences with their values in the app configs...")
+	err = configs.ReplaceEnvVarsInConfigs(appConfigs, env.Variables)
+	if err != nil {
+		fmt.Println("Failed to replace env variables occurrences with their values in the app configs")
+		return nil, nil, err
+	}
+	fmt.Printf("Processed app configs: %s\n", appConfigs)
+
+	return appConfigs, env, nil
+}
